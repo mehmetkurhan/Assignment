9ec32b6f480bf4c6a86226ccc30436a51d92a7da
Kerala Hough Brendon <keralabrendon@gmail.com>
2017-03-08 20:51:18 -0500

    Added comments to AutoShiftDecorator

--- Assignment/AutoShiftDecorator.java
+++ Assignment/AutoShiftDecorator.java
@@ -1,5 +1,6 @@
 package com.CarProject;
 
+//This class adds 2000 to the price and modifies fuel consumption
 public class AutoShiftDecorator extends Decorator {
 	
 	
@@ -9,10 +10,13 @@ public class AutoShiftDecorator extends Decorator {
 		super(_car);
 	}
 	
+	//Changes price of car
 	public int getPrice()
 	{
 		return car.getPrice() + 2000;
 	}
+	
+	//Change the fuel consumption based on the engine volume
 	public double getFuelConsumption(int velocity,double time)
 	{
 		if(car.getEngineVolume() < 2.0)
