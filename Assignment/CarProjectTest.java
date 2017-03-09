a0d7415c1443ff1613d55a4a3cff2fd5b98784cc
Kerala Hough Brendon <keralabrendon@gmail.com>
2017-03-08 19:48:52 -0500

    Added comments to CarProjectTest Class

--- Assignment/CarProjectTest.java
+++ Assignment/CarProjectTest.java
@@ -1,5 +1,6 @@
 package com.CarProject;
 
+// This is the base class of the program. 
 public class CarProjectTest {
 
 	public static void main(String[] args) {
@@ -7,14 +8,17 @@ public class CarProjectTest {
 				// TODO initiate factory!
 				ICarFactory factory = new CarFactory();
 				
-				ICar car1 = factory.produce("EA100");
+				ICar car1 = factory.produce("EA100"); // EA100 is the car we are getting information for
 				
+		
+				//Get these values from the Car class
 				System.out.println("Price: " + car1.getPrice());
 				System.out.println("Message: " + car1.getMessage(90));
 				System.out.println("Fuel Consumption: " + car1.getFuelConsumption(100, 1.2));
 				
+				//Increase the price by the appropriate amount based on airconditioning and automatic shift
 				car1 = CarDecorator.addAirConditioning(car1);
-				System.out.println("Price: " + car1.getPrice()); // Price should increase by 1000 TL
+				System.out.println("Price: " + car1.getPrice()); 
 
 				car1 = CarDecorator.addAutoShift(car1);
 				System.out.println("Price: " + car1.getPrice());
